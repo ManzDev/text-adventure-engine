fbe01accf72fa66ab3650f676217133b8da3f472
ManzDev <RomanSoftware@yahoo.es>
2014-01-24 04:02:54 +0000

    - Little button for future help.

--- index.php
+++ index.php
@@ -4,18 +4,22 @@
  <link href='http://fonts.googleapis.com/css?family=Open+Sans:200,400|Raleway:400|Press+Start+2P:400' rel='stylesheet' type='text/css'>
  <link type="text/css" rel="stylesheet" href="index.css" /> 
  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
- <title>JSGame</title>
+ <title>Innocent Engine</title>
  </head>
 <body>
 
 	<div id="page">
-		<h1>JSGame</h1>
-		<img id="img" src="" alt=" " />
+		<h1>Innocent Hill</h1>
+		<div id="img"><img src="" alt=" " /></div>
 		<div id="text"></div>
 		<div id="chat"></div>
 		<form onSubmit="doAction(document.getElementById('input').value); return false;">
 			<input type="text" id="input" value="" autocomplete="off" spellcheck="false" />
 		</form>
+		<div id="help">
+			<a onclick="show_help(); return false;">?</a>
+			<div class="help" style="display:none"></div>
+		</div>
 	</div>
 
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
